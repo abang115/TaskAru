58b86e11300e9a86163c6d7e8ad4d76788b27e2b
Alvin Bang <alvin.chijunbang@gmail.com>
2023-02-02 23:37:05 -0500

    Created home page component and set default routing to it

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'signin', component: SignInComponent},
-  { path: 'signup', component: SignUpComponent}
+  { path: 'signup', component: SignUpComponent},
+  { path: 'home', component: HomeComponent},
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
