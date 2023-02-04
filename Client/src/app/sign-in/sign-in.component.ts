0652cdcf07cdc9c02ed8736901f6e362ab8f0a1d
Alvin Bang <alvin.chijunbang@gmail.com>
2023-02-03 21:52:27 -0500

    Added email and password validation to signin form

--- Client/src/app/sign-in/sign-in.component.ts
+++ Client/src/app/sign-in/sign-in.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -9,7 +9,8 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class SignInComponent {
 
   signinForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
   })
+
 }
