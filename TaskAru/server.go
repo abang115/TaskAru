576d99c2f9d2f6a2265dc9b28cff31a50c955193
Amanda Yu <92806914+yuamanda@users.noreply.github.com>
2023-02-05 17:59:10 -0500

    Add files via upload

--- TaskAru/server.go
+++ TaskAru/server.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var client *redis.Client
@@ -19,24 +20,32 @@ func main() {
 	})
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
-	//r.HandleFunc("/", homeHandler)
-	//r.HandleFunc("/createAccount", createAccountHandler)
+	r.HandleFunc("/", homeGetHandler).Methods("GET")
+	r.HandleFunc("/", homePostHandler).Methods("POST")
+	r.HandleFunc("/register", registerGetHandler).Methods("GET")
+	r.HandleFunc("/register", registerPostHandler).Methods("POST")
 	r.HandleFunc("/login", loginGetHandler).Methods("GET")
 	r.HandleFunc("/login", loginPostHandler).Methods("POST")
-	r.HandleFunc("/test", testGetHandler).Methods("GET")
+	//r.HandleFunc("/test", testGetHandler).Methods("GET")
 	fs := http.FileServer(http.Dir("./static/"))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
 
-// func homeHandler() {
-// 	templates.ExecuteTemplate(w, "placeholder.html", nil)
-// }
+func homeGetHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session")
+	_, ok := session.Values["username"]
+	if !ok {
+		http.Redirect(w, r, "/login", 302)
+		return
+	}
+	templates.ExecuteTemplate(w, "home.html", nil)
+}
 
-// func createAccountHandler(w http.ResponseWriter, r *http.Request) {
-// 	templates.ExecuteTemplate(w, "placeholder.html", nil)
-// }
+func homePostHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/", 302)
+}
 
 func loginGetHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "login.html", nil)
@@ -45,20 +54,47 @@ func loginGetHandler(w http.ResponseWriter, r *http.Request) {
 func loginPostHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	username := r.PostForm.Get("username")
+	password := r.PostForm.Get("password")
+	hash, err := client.Get("user" + username).Bytes()
+	if err != nil {
+		return
+	}
+	err = bcrypt.CompareHashAndPassword(hash, []byte(password))
+	if err != nil {
+		return
+	}
 	session, _ := store.Get(r, "session")
 	session.Values["username"] = username
 	session.Save(r, w)
+	http.Redirect(w, r, "/", 302)
 }
 
-func testGetHandler(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "session")
-	untyped, ok := session.Values["username"]
-	if !ok {
+// func testGetHandler(w http.ResponseWriter, r *http.Request) {
+// 	session, _ := store.Get(r, "session")
+// 	untyped, ok := session.Values["username"]
+// 	if !ok {
+// 		return
+// 	}
+// 	username, ok := untyped.(string)
+// 	if !ok {
+// 		return
+// 	}
+// 	w.Write([]byte(username))
+// }
+
+func registerGetHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "register.html", nil)
+}
+
+func registerPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	username := r.PostForm.Get("username")
+	password := r.PostForm.Get("password")
+	cost := bcrypt.DefaultCost
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), cost)
+	if err != nil {
 		return
 	}
-	username, ok := untyped.(string)
-	if !ok {
-		return
-	}
-	w.Write([]byte(username))
+	client.Set("user:" + username, hash, 0)
+	http.Redirect(w, r, "/login", 302)
 }
