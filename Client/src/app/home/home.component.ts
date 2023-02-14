c5c67ed62e0f901d13f2d164e1e0712eddf7950e
Alvin Bang <alvin.chijunbang@gmail.com>
2023-02-13 22:52:15 -0500

    Added HttpClient module support to signin and signup

--- Client/src/app/home/home.component.ts
+++ Client/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
 }
--- Client/src/app/sign-up/sign-up.component.ts
+++ Client/src/app/sign-up/sign-up.component.ts
@@ -1,36 +1,60 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, AbstractControl, ValidatorFn, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit{
 
-signupForm = new FormGroup({
+  constructor(private formBuilder: FormBuilder, public http: HttpClient) { }
+
+  signupForm: FormGroup = this.formBuilder.group({
     firstName: new FormControl('', [Validators.required]),
     lastName: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
     confirmPassword: new FormControl('', [Validators.required]),
- }, {validators: [this.match('password', 'confirmPassword')]});
+  }, {validators: [this.match('password', 'confirmPassword')]});;
 
- match(controlName: string, checkControlName: string): ValidatorFn {
-  return (controls: AbstractControl) => {
-    const control = controls.get(controlName);
-    const checkControl = controls.get(checkControlName);
+  ngOnInit(): void {
+  }
 
-    if (checkControl?.errors && !checkControl.errors['matching']) {
-      return null;
-    }
+  match(controlName: string, checkControlName: string): ValidatorFn {
+    return (controls: AbstractControl) => {
+      const control = controls.get(controlName);
+      const checkControl = controls.get(checkControlName);
 
-    if (control?.value !== checkControl?.value) {
-      controls.get(checkControlName)?.setErrors({ matching: true });
-      return { matching: true };
-    } else {
-      return null;
+      if (checkControl?.errors && !checkControl.errors['matching']) {
+        return null;
+      }
+
+      if (control?.value !== checkControl?.value) {
+        controls.get(checkControlName)?.setErrors({ matching: true });
+        return { matching: true };
+      } else {
+        return null;
+      }
     }
   }
-}
+
+  register(){
+    const user = {
+      firstName: this.signupForm.get('firstName')!.value, 
+      lastName: this.signupForm.get('lastName')!.value,
+      email: this.signupForm.get('email')!.value, 
+      password: this.signupForm.get('password')!.value
+    }
+    
+    this.http.post('http://localhost:8080/register', user).subscribe({
+        next: response => {
+          console.log('User successfully created', response)
+        },
+        error: err => {
+          console.error('Error creating user: ', err)
+        }
+      });
+  }
 }
