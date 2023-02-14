c2beb98c92ad638ba339eaa6d50a90f519e16911
EricWang228 <99090141+EricWang228@users.noreply.github.com>
2023-02-14 15:54:56 -0500

    Added custom events functionality to Calendar object

--- Client/src/app/calendar/calendar.component.ts
+++ Client/src/app/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, TemplateRef,ChangeDetectorRef  } from "@angular/core";
+import { Component, OnInit, AfterViewInit, ViewChild, ChangeDetectorRef, ElementRef  } from "@angular/core";
 import { CalendarOptions, DateSelectArg, EventClickArg, EventApi, Calendar } from '@fullcalendar/core';
 import { INITIAL_EVENTS, createEventId } from "./event.utils";
 import interactionPlugin from '@fullcalendar/interaction';
@@ -7,80 +7,93 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import { BsModalRef, BsModalService } from "ngx-bootstrap/modal";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { FullCalendarComponent } from "@fullcalendar/angular";
 
 @Component({
   selector: 'calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
-export class CalendarComponent implements OnInit{
+export class CalendarComponent implements OnInit, AfterViewInit  {
   modalRef?: BsModalRef;
   calendarVisible = true;
   currentEvents: EventApi[] = [];
   title: any;
   start: any;
-  calendarOptions: CalendarOptions = {
-    plugins: [
-      interactionPlugin,
-      dayGridPlugin,
-      timeGridPlugin,
-      listPlugin,
-    ],
-    customButtons:{
-      myCustomButton:{
-        text: 'Add Event',
-        click: this.handleAddEventClick.bind(this)
-      }
-    },
-    headerToolbar: {
-      left: 'prev,next today myCustomButton',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-    },
-    initialView: 'dayGridMonth',
-    initialEvents: INITIAL_EVENTS, 
-    weekends: true,
-    editable: true,
-    selectable: true,
-    selectMirror: true,
-    dayMaxEvents: true,
-    eventClick: this.handleDateClick.bind(this),
-  };
+  calendarOptions: any;
   
   config ={
     animated: true
   };
 
-  @ViewChild('addEventModal') addEventModal!: string;
+  @ViewChild('eventModal') eventModal!: string;
   @ViewChild('template') template!: string;
+  @ViewChild('cal') fullCalendarComponent!:FullCalendarComponent;
 
-  constructor(private modalService: BsModalService) { }
+  constructor(private modalService: BsModalService, private el: ElementRef) {     }
 
   ngOnInit(): void {
-    this.calendarOptions = this.calendarOptions;
+    this.calendarOptions = {
+      plugins: [
+        interactionPlugin,
+        dayGridPlugin,
+        timeGridPlugin,
+        listPlugin,
+      ],
+      customButtons:{
+        myCustomButton:{
+          text: 'Add Event',
+          click: this.handleAddEventClick.bind(this)
+        }
+      },
+      headerToolbar: {
+        left: 'prev,next today myCustomButton',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+      },
+      initialView: 'dayGridMonth',
+      initialEvents: INITIAL_EVENTS, 
+      weekends: true,
+      editable: true,
+      selectable: true,
+      selectMirror: true,
+      dayMaxEvents: true,
+      eventClick: this.handleDateClick.bind(this),
+    };
+    this.addEvent();
+    }
+
+  ngAfterViewInit(): void{
+    console.log(this.fullCalendarComponent);
   }
 
   handleDateClick(arg:any){
     console.log(arg);
     console.log(arg.event._def.title);
     this.title = arg.event.def_title;
-    
     this.modalRef = this.modalService.show(this.template, this.config);
   }
-  
+
   handleAddEventClick(){
     console.log('add events clicked');
-    this.modalRef = this.modalService.show(this.addEventModal);
+    this.modalRef = this.modalService.show(this.eventModal);
   }
 
   addEvent(){
     console.log('add events clicked');
-
+    console.log(this.addEventForm.value);
+    let newEvent = {
+      id: createEventId(),
+      title: this.addEventForm.value.eventTitle || '',
+      start: this.addEventForm.value.eventDate || '',
+    }
+    this.fullCalendarComponent.getApi().addEvent(newEvent);
+    this.modalRef?.hide();  
   }
 
   addEventForm = new FormGroup({
-    eventTitle: new FormControl('',[Validators.required]),
-    eventDate: new FormControl('',[Validators.required]),
+    eventTitle: new FormControl('', [Validators.required]),
+    eventDate: new FormControl('', [Validators.required]),
     // startTime: new FormControl(''),
     // endTime: new FormControl(''),
     // reoccuring: new FormControl(''),
