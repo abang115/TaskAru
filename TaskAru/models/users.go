2d0db7ad4bdd5d8953c10a9bc942f238d6b85dd1
Amanda Yu <92806914+yuamanda@users.noreply.github.com>
2023-02-20 17:39:42 -0500

    Update users.go

--- TaskAru/models/users.go
+++ TaskAru/models/users.go
@@ -1,5 +1,12 @@
 package models
 
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+	//"github.com/CEN3031/TaskAru/models"
+)
+
 type User struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
@@ -10,4 +17,22 @@ type User struct {
 type userLogin struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+func CreateUser(user User) (User, error) {
+	// Hash Password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return User{}, errors.New("could not hash password")
+	}
+
+	user.Password = string(hashedPassword)
+	result := DB.Where("email = ?", user.Email).FirstOrCreate(&user)
+
+	// User already exists
+	if result.RowsAffected == 0 {
+		return User{}, errors.New("User Already Exists")
+	}
+
+	return user, nil
 }
