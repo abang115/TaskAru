e0eb32670aa5c81c3dd6d2bb4b029e59d0c9776c
Amanda Yu <92806914+yuamanda@users.noreply.github.com>
2023-02-20 21:13:11 -0500

    added create user function

--- TaskAru/controllers/user.go
+++ TaskAru/controllers/user.go
@@ -1,11 +1,55 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
+
+	// "errors"
+	// "fmt"
+	// "strconv"
+	// "github.com/gorilla/mux"
+	// "golang.org/x/crypto/bcrypt"
+	// "gorm.io/gorm"
+	"TaskAru/models"
 )
 
 func RegisterPostHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "*")
 	w.WriteHeader(http.StatusOK)
 
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	// checks to see if first name, last name, password, and email fields are present
+	if user.FirstName == "" || user.LastName == "" || user.Password == "" || user.Email == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Request Body Missing Fields"))
+		return
+	}
+
+	// creates user
+	userCreated, userErr := models.CreateUser(user)
+
+	if userErr != nil {
+		// if password can't be hashed
+		if userErr.Error() == "could not hash password" {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte(userErr.Error()))
+			return
+		}
+
+		// if the user already exists
+		if userErr.Error() == "User Already Exists" {
+			w.WriteHeader(http.StatusConflict)
+			w.Write([]byte(userErr.Error()))
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode(userCreated)
 }
