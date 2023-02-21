8fdcd051e5eb41654dd3b990b6da081d24413ce0
EricWang228 <99090141+EricWang228@users.noreply.github.com>
2023-02-21 01:05:30 -0500

    Added timed event functionality

--- Client/src/app/calendar/event.utils.ts
+++ Client/src/app/calendar/event.utils.ts
@@ -1,4 +1,5 @@
 import { EventInput } from '@fullcalendar/core';
+import * as moment from 'moment'; // import moment library
 
 let eventGuid = 0;
 const TODAY_STR = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
@@ -25,4 +26,11 @@ export const INITIAL_EVENTS: EventInput[] = [
 
 export function createEventId() {
   return String(eventGuid++);
+}
+
+export function toEventFormat(date:any, time:any){
+  let modifiedDate = moment(date).format('YYYY-MM-DD');
+  let modifiedTime = moment(time, 'hh:mm A').format('HH:mm:ss');
+  let finishedFormat = modifiedDate + 'T' + modifiedTime;
+  return finishedFormat;
 }
