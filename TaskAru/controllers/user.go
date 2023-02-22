a96c4a698aee3b9f990ea8b5f42772606b2102bb
Alvin Bang <alvin.chijunbang@gmail.com>
2023-02-22 18:16:49 -0500

    Deleted redundant CreateUser function

--- TaskAru/controllers/user.go
+++ TaskAru/controllers/user.go
@@ -38,38 +38,3 @@ func RegisterPostHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(newUser)
 }
-
-func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	var user models.User
-	json.NewDecoder(r.Body).Decode(&user)
-
-	// checks to see if first name, last name, password, and email fields are present
-	if user.FirstName == "" || user.LastName == "" || user.Password == "" || user.Email == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Request Body Missing Fields"))
-		return
-	}
-
-	// creates user
-	userCreated, userErr := models.CreateUser(user)
-
-	if userErr != nil {
-		// if password can't be hashed
-		if userErr.Error() == "could not hash password" {
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte(userErr.Error()))
-			return
-		}
-
-		// if the user already exists
-		if userErr.Error() == "User Already Exists" {
-			w.WriteHeader(http.StatusConflict)
-			w.Write([]byte(userErr.Error()))
-			return
-		}
-	}
-
-	json.NewEncoder(w).Encode(userCreated)
-}
--- TaskAru/models/users.go
+++ TaskAru/models/users.go
@@ -1,12 +1,5 @@
 package models
 
-import (
-	"errors"
-
-	"golang.org/x/crypto/bcrypt"
-	//"github.com/CEN3031/TaskAru/models"
-)
-
 type User struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
@@ -17,22 +10,4 @@ type User struct {
 type userLogin struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
-}
-
-func CreateUser(user User) (User, error) {
-	// hash passwords
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-	if err != nil {
-		return User{}, errors.New("could not hash password")
-	}
-
-	user.Password = string(hashedPassword)
-	result := DB.Where("email = ?", user.Email).FirstOrCreate(&user)
-
-	// if the user already exists
-	if result.RowsAffected == 0 {
-		return User{}, errors.New("User Already Exists")
-	}
-
-	return user, nil
 }
