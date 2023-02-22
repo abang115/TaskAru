db7f598fe5570afc93a38fa816a3468fab58d4d6
Alvin Bang <alvin.chijunbang@gmail.com>
2023-02-21 20:21:06 -0500

    Successfully connected front and back end, allowing user signup

--- TaskAru/controllers/user.go
+++ TaskAru/controllers/user.go
@@ -1,11 +1,40 @@
 package controllers
 
 import (
+	"TaskAru/models"
+	"encoding/json"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func RegisterPostHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "*")
 	w.WriteHeader(http.StatusOK)
 
+	var newUser models.User
+	var users []models.User
+	models.DB.Find(&users)
+	_ = json.NewDecoder(r.Body).Decode(&newUser)
+
+	for _, entry := range users {
+		if entry.Email == newUser.Email {
+			w.WriteHeader(http.StatusConflict)
+			errorMessage := map[string]string{"error": "Email already exists"}
+			json.NewEncoder(w).Encode(errorMessage)
+			return
+		}
+	}
+
+	password, err := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		errorMessage := map[string]string{"error": "could not hash password"}
+		json.NewEncoder(w).Encode(errorMessage)
+		return
+	}
+	newUser.Password = string(password)
+	models.DB.Create(&newUser)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(newUser)
 }
