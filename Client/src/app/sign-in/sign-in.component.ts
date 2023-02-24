8501f5ecbfe81718e38761a22d9c90a3240bf797
Alvin Bang <alvin.chijunbang@gmail.com>
2023-02-23 21:19:29 -0500

    Made it so that sign-in and sign-up buttons are only visible when the user has not signed in

--- Client/src/app/sign-in/sign-in.component.ts
+++ Client/src/app/sign-in/sign-in.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators, FormBuilder,} from '@angular/forms';
 import { Router } from '@angular/router'
 import { HttpClient } from '@angular/common/http';
+import { SignInService } from '../sign-in.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -11,7 +12,7 @@ import { HttpClient } from '@angular/common/http';
 
 export class SignInComponent implements OnInit{
 
-  constructor(private formBuilder: FormBuilder, public http: HttpClient, private router: Router) { }
+  constructor(private formBuilder: FormBuilder, public http: HttpClient, private router: Router, public signInService: SignInService) { }
 
   signinForm: FormGroup = this.formBuilder.group({
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -32,6 +33,7 @@ export class SignInComponent implements OnInit{
       this.http.post('http://localhost:8080/signin', userSignIn).subscribe({
         next: response => {
           console.log('Backend successfully reached: ', response)
+          this.signInService.signIn()
           setTimeout(() => {
             this.router.navigate(['/home']);
           }, 2000);
