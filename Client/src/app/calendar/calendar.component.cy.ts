14b3ec410dcb542103f7aa0a49df1b70567bebe9
Alvin Bang <alvin.chijunbang@gmail.com>
2023-02-26 22:46:37 -0500

    Created calendar component test with mounting test

---
+++ Client/src/app/calendar/calendar.component.cy.ts
@@ -0,0 +1,18 @@
+import { CalendarComponent } from "./calendar.component";
+import { BsModalRef, BsModalService } from "ngx-bootstrap/modal";
+import { FullCalendarComponent } from "@fullcalendar/angular";
+
+
+describe('CalendarComponent', () => {
+    it('should mount', () => {
+      cy.mount(CalendarComponent, {
+        imports: [],
+        providers:[
+          BsModalRef,
+          BsModalService,
+        ],
+        declarations: [FullCalendarComponent],
+      })
+      cy.viewport(1000, 850)
+    });
+})
