2792c7929636ac8a96ef27a5c975325d9d8896c8
Alvin Bang <alvin.chijunbang@gmail.com>
2023-02-26 19:04:20 -0500

    Added buttons for light mode and dark mode toggle

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field'; 
 import {MatInputModule} from '@angular/material/input'; 
 import {MatCheckboxModule} from '@angular/material/checkbox'; 
+import {MatButtonToggleModule} from '@angular/material/button-toggle'; 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -19,6 +20,7 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { HomeComponent } from './home/home.component';
 import { FullCalendarModule } from '@fullcalendar/angular';
 import { CalendarComponent } from './calendar/calendar.component'; 
+import {MatIcon, MatIconModule} from '@angular/material/icon'; 
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { RouterModule } from '@angular/router';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
@@ -43,6 +45,8 @@ import { ResetPasswordComponent } from './reset-password/reset-password.componen
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
+    MatIconModule,
+    MatButtonToggleModule,
     ReactiveFormsModule,
     FullCalendarModule,
     ModalModule.forRoot(), 
