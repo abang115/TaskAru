04709cd35df1fee193d606aa1fc8b24937018f84
Alvin Bang <alvin.chijunbang@gmail.com>
2023-02-28 16:26:05 -0500

    Setup more mounting tests for signin and signup components

---
+++ Client/src/app/sign-in/sign-in.component.cy.ts
@@ -0,0 +1,31 @@
+import { SignInComponent } from './sign-in.component';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { Router } from '@angular/router'
+import { HttpClientModule } from '@angular/common/http';
+import { SignInService } from '../sign-in.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+describe('SignUpComponent', () => {
+    it('should mount', () => {
+      cy.mount(SignInComponent, {
+        imports: [
+          FormsModule,
+          ReactiveFormsModule,
+          HttpClientModule,
+          MatButtonModule,
+          MatCheckboxModule,
+          MatFormFieldModule,
+          MatInputModule,
+          BrowserAnimationsModule,
+        ],
+        providers:[
+          Router, 
+          SignInService,
+        ],
+      })
+    });
+})
