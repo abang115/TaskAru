186604fd2782f8e562af79ce786e8bd6375169ac
EricWang228 <99090141+EricWang228@users.noreply.github.com>
2023-02-28 17:59:16 -0500

    Added test case for new events

--- Client/src/app/calendar/calendar.component.cy.ts
+++ Client/src/app/calendar/calendar.component.cy.ts
@@ -7,16 +7,65 @@ import { FullCalendarComponent } from "@fullcalendar/angular";
 describe('CalendarComponent', () => {
     it('should mount', () => {
       cy.mount(CalendarComponent, {
-        imports: [
-          FormsModule,
-          ReactiveFormsModule,
-        ],
+        imports: [],
         providers:[
           BsModalRef,
           BsModalService,
         ],
-        declarations: [FullCalendarComponent,],
+        declarations: [
+          FullCalendarComponent,
+          CalendarComponent,
+        ],
       })
       cy.viewport(1000, 850)
     });
-})
+
+    it('Modal Opens up', () =>{
+      cy.mount(CalendarComponent, {
+        imports: [ReactiveFormsModule],
+        providers:[
+          BsModalRef,
+          BsModalService,
+        ],
+        declarations: [
+          FullCalendarComponent,
+          CalendarComponent,
+        ],
+      }).then( ()=>{
+        cy.get('.fc-myCustomButton-button.fc-button.fc-button-primary').click(); 
+        cy.get('.modal-body').should('be.visible');
+      })
+    });
+
+    it('Create new event',() =>{
+      cy.mount(CalendarComponent, {
+        imports: [ReactiveFormsModule],
+        providers:[
+          BsModalRef,
+          BsModalService,
+        ],
+        declarations: [
+          FullCalendarComponent,
+          CalendarComponent,
+        ],
+      }).then( ()=> {
+        // Add Event Buttom
+        cy.get('.fc-myCustomButton-button.fc-button.fc-button-primary').click(); 
+        // Make sure modal pops up
+        cy.wait(100);
+        // Fill out form fields
+        cy.get('#event-title').type('Test Event');
+        cy.get('#event-date').type('2023-02-27');
+        cy.get('#event-start-time').type('11:00');
+        cy.get('#event-end-time').type('12:00');
+        cy.get('#event-reoccuring').select('once');
+        cy.get('#event-description').type('This is a test');
+        cy.wait(100);
+        cy.get('#event-button-submit').click();
+        // Make sure new event shows up
+        cy.wait(100)
+        // Check if new event is inserted
+        cy.get('.fc-daygrid-day-events').should('contain', 'Test Event');
+      })
+    });
+})  
