ebb32b90b992f37e69259f986abb3fd48ea95070
Alvin Bang <alvin.chijunbang@gmail.com>
2023-03-01 20:56:00 -0500

    Update server_test.go

--- TaskAru/server_test.go
+++ TaskAru/server_test.go
@@ -26,8 +26,8 @@ func deleteFromUsersTable() {
 
 func TestMain(m *testing.M) {
 	models.Init("taskarudb_test")
-	testRouter.HandleFunc("/register", controllers.RegisterPostHandler).Methods("POST")
-	testRouter.HandleFunc("/signin", controllers.SignInPostHandler).Methods("POST")
+	testRouter.HandleFunc("/api/register", controllers.RegisterPostHandler).Methods("POST")
+	testRouter.HandleFunc("/api/signin", controllers.SignInPostHandler).Methods("POST")
 
 	test := m.Run()
 	os.Exit(test)
@@ -35,10 +35,11 @@ func TestMain(m *testing.M) {
 
 // test to register successfully with email and password
 func TestRegisterPostHandler(t *testing.T) {
+	deleteFromUsersTable()
 	rBody := []byte(`{"first_name": "jane", "last_name": "doe", "email": "janedoe@ufl.edu", "password": "janedoe"}`)
 
 	rr := httptest.NewRecorder()
-	req := httptest.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(rBody))
+	req := httptest.NewRequest(http.MethodPost, "/api/register", bytes.NewBuffer(rBody))
 	testRouter.ServeHTTP(rr, req)
 
 	var user models.User
@@ -55,7 +56,7 @@ func TestRegisterPostHandler(t *testing.T) {
 	assert.Equal(t, "jane", user.FirstName, "incorrect first name error")
 	assert.Equal(t, "doe", user.LastName, "incorrect last name error")
 	assert.Equal(t, "janedoe@ufl.edu", user.Email, "incorrect email error")
-	assert.Equal(t, http.StatusOK, rr.Code, "HTTP request status code error")
+	assert.Equal(t, http.MethodPost, req.Method, "HTTP request method error")
 	assert.Equal(t, http.StatusOK, rr.Code, "HTTP request status code error")
 }
 
@@ -65,7 +66,7 @@ func Test2RegisterPostHandler(t *testing.T) {
 
 	rBody := []byte(`{"first_name": "jane", "last_name": "doe", "email": "janedoe@ufl.edu", "password": "janedoe"}`)
 	rr := httptest.NewRecorder()
-	req := httptest.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(rBody))
+	req := httptest.NewRequest(http.MethodPost, "/api/register", bytes.NewBuffer(rBody))
 	testRouter.ServeHTTP(rr, req)
 
 	var user models.User
@@ -82,12 +83,12 @@ func Test2RegisterPostHandler(t *testing.T) {
 	assert.Equal(t, "jane", user.FirstName, "incorrect first name error")
 	assert.Equal(t, "doe", user.LastName, "incorrect last name error")
 	assert.Equal(t, "janedoe@ufl.edu", user.Email, "incorrect email error")
-	assert.Equal(t, http.StatusOK, rr.Code, "HTTP request status code error")
+	assert.Equal(t, http.MethodPost, req.Method, "HTTP request method error")
 	assert.Equal(t, http.StatusOK, rr.Code, "HTTP request status code error")
 
 	rBody = []byte(`{"first_name": "jane2", "last_name": "doe2", "email": "janedoe@ufl.edu", "password": "janedoe2"}`)
 	rr = httptest.NewRecorder()
-	req = httptest.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(rBody))
+	req = httptest.NewRequest(http.MethodPost, "/api/register", bytes.NewBuffer(rBody))
 	testRouter.ServeHTTP(rr, req)
 
 	result = models.DB.Where("email = ?", "janedoe@ufl.edu").First(&user)
@@ -104,7 +105,7 @@ func Test2RegisterPostHandler(t *testing.T) {
 	assert.Equal(t, "doe", user.LastName, "incorrect last name error")
 	assert.Equal(t, "janedoe@ufl.edu", user.Email, "incorrect email error")
 	assert.Equal(t, http.MethodPost, req.Method, "HTTP request method error")
-	assert.Equal(t, http.StatusOK, rr.Code, "HTTP request status code error")
+	assert.Equal(t, http.StatusConflict, rr.Code, "HTTP request status code error")
 }
 
 // test to signin with correct email and password
@@ -112,7 +113,7 @@ func TestSignInPostHandler(t *testing.T) {
 	rBody := []byte(`{"email": "janedoe@ufl.edu", "password": "janedoe"}`)
 
 	rr := httptest.NewRecorder()
-	req := httptest.NewRequest(http.MethodPost, "/signin", bytes.NewBuffer(rBody))
+	req := httptest.NewRequest(http.MethodPost, "/api/signin", bytes.NewBuffer(rBody))
 	testRouter.ServeHTTP(rr, req)
 
 	var user models.User
@@ -138,7 +139,7 @@ func TestSignInPostHandler2(t *testing.T) {
 	rBody := []byte(`{"email": "janedoe@ufl.edu", "password": "janedoe2"}`)
 
 	rr := httptest.NewRecorder()
-	req := httptest.NewRequest(http.MethodPost, "/signin", bytes.NewBuffer(rBody))
+	req := httptest.NewRequest(http.MethodPost, "/api/signin", bytes.NewBuffer(rBody))
 	testRouter.ServeHTTP(rr, req)
 
 	var user models.User
@@ -161,7 +162,7 @@ func TestSignInPostHandler3(t *testing.T) {
 	rBody := []byte(`{"email": "janedoe2@ufl.edu", "password": "janedoe"}`)
 
 	rr := httptest.NewRecorder()
-	req := httptest.NewRequest(http.MethodPost, "/signin", bytes.NewBuffer(rBody))
+	req := httptest.NewRequest(http.MethodPost, "/api/signin", bytes.NewBuffer(rBody))
 	testRouter.ServeHTTP(rr, req)
 
 	var user models.User
