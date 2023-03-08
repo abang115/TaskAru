d4d8c8c1e66611757da4708a00fd326d639e59f2
yuamanda <92806914+yuamanda@users.noreply.github.com>
2023-03-08 14:28:52 -0500

    edit event post handler function started

--- TaskAru/controllers/event.go
+++ TaskAru/controllers/event.go
@@ -16,3 +16,33 @@ func EventPostHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(newEvent)
 }
+
+func EditEventPostHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "*")
+	w.WriteHeader(http.StatusOK)
+
+	// gets the ID of the event that needs to be edited
+	id := r.URL.Query().Get("id")
+
+	// does it need to be newEvent?
+	var updateEvent models.Event
+	var events []models.Event
+
+	searchErr := models.DB.Where("eventID = ?", updateEvent.EventID).First(&events, id).Error
+
+	if searchErr != nil {
+		w.WriteHeader(http.StatusNotFound)
+		errorMessage := map[string]string{"error": "could not find event"}
+		json.NewEncoder(w).Encode(errorMessage)
+		return
+	}
+
+	if err := models.DB.Where("eventID = ?", updateEvent.EventID).Updates(models.Event{EventTitle: updateEvent.EventTitle, Description: updateEvent.Description, EventDate: updateEvent.EventDate, StartTime: updateEvent.StartTime, EndTime: updateEvent.EndTime}).Error; err != nil {
+		// check error message
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(updateEvent)
+}
