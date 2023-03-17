94ebb03ccc933bb3fa217f7c7f13b81694a1cc3a
Alvin Bang <alvin.chijunbang@gmail.com>
2023-03-17 14:10:59 -0400

    Created front-end for reset password form

--- Client/src/app/reset-password/reset-password.component.ts
+++ Client/src/app/reset-password/reset-password.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, AbstractControl, ValidatorFn, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ThemeService } from '../theme.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,5 +10,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent {
-  
+  darkMode: boolean = false
+  constructor(private formBuilder: FormBuilder, public http: HttpClient, private router: Router, private themeService: ThemeService) { }
+
+  resetForm: FormGroup = this.formBuilder.group({
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required]),
+  }, {validators: [this.match('password', 'confirmPassword')]});;
+
+  ngOnInit(): void {
+    this.themeService.darkMode$.subscribe((themeStatus: boolean) => {
+      this.darkMode = themeStatus
+    }); 
+  }
+
+  match(controlName: string, checkControlName: string): ValidatorFn {
+    return (controls: AbstractControl) => {
+      const control = controls.get(controlName);
+      const checkControl = controls.get(checkControlName);
+
+      if (checkControl?.errors && !checkControl.errors['matching']) {
+        return null;
+      }
+
+      if (control?.value !== checkControl?.value) {
+        controls.get(checkControlName)?.setErrors({ matching: true });
+        return { matching: true };
+      } else {
+        return null;
+      }
+    }
+  }
 }
