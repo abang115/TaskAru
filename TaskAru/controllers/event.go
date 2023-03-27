d27b7a5639fac8198124c813b23d1eb13e64298a
yuamanda <92806914+yuamanda@users.noreply.github.com>
2023-03-27 11:22:35 -0400

    created RemoveEventDeleteHandler function

--- TaskAru/controllers/event.go
+++ TaskAru/controllers/event.go
@@ -40,7 +40,7 @@ func EditEventPatchHandler(w http.ResponseWriter, r *http.Request) {
 	if err := models.DB.Where("eventID = ?", updateEvent.EventID).Updates(models.Event{EventTitle: updateEvent.EventTitle, Description: updateEvent.Description, EventDate: updateEvent.EventDate, StartTime: updateEvent.StartTime, EndTime: updateEvent.EndTime, Freq: updateEvent.Freq, DTStart: updateEvent.DTStart, Until: updateEvent.Until}).Error; err != nil {
 		// check error message
 		w.WriteHeader(http.StatusInternalServerError)
-		errorMessage := map[string]string{"error": "could not update evnet"}
+		errorMessage := map[string]string{"error": "could not update event"}
 		json.NewEncoder(w).Encode(errorMessage)
 		return
 	}
@@ -49,9 +49,38 @@ func EditEventPatchHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(updateEvent)
 }
 
-// func RemoveEventDeleteHandler(w http.ResponseWriter, r *http.Request) {
+func RemoveEventDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "*")
+	w.WriteHeader(http.StatusOK)
 
-// }
+	// gets the ID of the event that needs to be deleted
+	id := r.URL.Query().Get("id")
+
+	// does it need to be newEvent?
+	var deleteEvent models.Event
+	var events []models.Event
+
+	searchErr := models.DB.Where("eventID = ?", deleteEvent.EventID).First(&events, id).Error
+
+	if searchErr != nil {
+		w.WriteHeader(http.StatusNotFound)
+		errorMessage := map[string]string{"error": "could not find event"}
+		json.NewEncoder(w).Encode(errorMessage)
+		return
+	}
+
+	if err := models.DB.Where("eventID = ?", deleteEvent.EventID).Delete(&deleteEvent).Error; err != nil {
+
+		// check error message
+		w.WriteHeader(http.StatusInternalServerError)
+		errorMessage := map[string]string{"error": "could not delete event"}
+		json.NewEncoder(w).Encode(errorMessage)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(deleteEvent)
+}
 
 // func ReceiveEventGetHandler(w http.ResponseWriter, r *http.Request) {
 
--- TaskAru/models/events.go
+++ TaskAru/models/events.go
@@ -1,30 +1,15 @@
 package models
 
-// import(
-// 	"image/color"
-// )
-
-// type RRule struct {
-// 	Freq    string `json:"freq"`
-// 	DTStart string `json:"dtstart"`
-// 	Until   string `json:"until"`
-// }
-
 type Event struct {
-	Email       string `json:"email"`
-	EventID     string `json:"eventID"`
-	EventTitle  string `json:"eventTitle"`
-	Description string `json:"eventDescription"`
-	EventDate   string `json:"eventDate"`
-	StartTime   string `json:"startTime"`
-	EndTime     string `json:"endTime"`
-
-	// subject to change
+	Email           string `json:"email"`
+	EventID         string `json:"eventID"`
+	EventTitle      string `json:"eventTitle"`
+	Description     string `json:"eventDescription"`
+	EventDate       string `json:"eventDate"`
+	StartTime       string `json:"startTime"`
+	EndTime         string `json:"endTime"`
 	Freq            string `json:"freq"`
 	DTStart         string `json:"dtstart"`
 	Until           string `json:"until"`
 	BackgroundColor string `json:"backgroundColor"`
-
-	// rrule	    string `json:rrule"`
-	// BackgroundColor color.RGBA
 }
