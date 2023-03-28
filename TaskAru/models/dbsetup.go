33a0c72770b32d53b42ec981f6ee299f6c0af79e
Amanda Yu <92806914+yuamanda@users.noreply.github.com>
2023-03-28 11:00:39 -0400

    Update dbsetup.go

--- TaskAru/models/dbsetup.go
+++ TaskAru/models/dbsetup.go
@@ -31,7 +31,7 @@ func Init(dbname string) {
 	}
 	log.Println("Connected to DB")
 
-	err = database.AutoMigrate(&User{}, &ForgotPassword{}, &Event{}) // &Event{} Add back in
+	err = database.AutoMigrate(&User{}, &ForgotPassword{}, &Event{})
 	if err != nil {
 		return
 	}
