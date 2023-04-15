f321b345fdc23c98fbcecb5d348dce2bc4952b9e
Alvin Bang <alvin.chijunbang@gmail.com>
2023-04-15 16:09:48 -0400

    Created notifications drop down

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import {MatToolbarModule} from '@angular/material/toolbar'; 
 import {MatButtonModule} from '@angular/material/button'; 
+import { MatMenuModule } from '@angular/material/menu';
 import {MatCardModule} from '@angular/material/card'; 
 import {MatFormFieldModule} from '@angular/material/form-field'; 
 import {MatInputModule} from '@angular/material/input'; 
@@ -55,6 +56,7 @@ import { ForgotPasswordComponent } from './forgot-password/forgot-password.compo
     ModalModule.forRoot(), 
     MatCheckboxModule,
     RouterModule,
+    MatMenuModule,
   ],
   providers: [JwtHelperService],
   bootstrap: [AppComponent]
